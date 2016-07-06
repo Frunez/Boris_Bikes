d78d3342eae95c3098ed304827d1b0eaa8ef8f8f
Frunez <francesco.nunez@gmail.com>
2016-07-06 16:33:52 +0100

    Making doc stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
 
   it { expect(DockingStation.new.respond_to? "release_bike").to eq true }
 
+describe Bike do
+  it {expect(Bike.new.respond_to? "working?").to eq true}
+end
 end
