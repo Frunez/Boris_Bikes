34c2b419bb2eb9749dc084bd7473b74e012db848
Frunez <francesco.nunez@gmail.com>
2016-07-08 12:25:31 +0100

    Challenge 15 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,30 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.count == 20
+    fail 'Docking station is full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    if @bikes.count >= 20
+      true
+    else
+      false
+    end
+  end
+
+  def empty?
+    if @bikes.empty?
+      true
+    else
+      false
+    end
   end
 end
